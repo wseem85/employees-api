49aed666730f2ba17933f62e5c190f67b593fe3a
wseem85 <engwseem2@gmail.com>
2024-08-24 13:51:09 +0300

    adding some functionalities

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ app.use(express.json());
 const PORT =process.env.PORT || 8000;
 const employees = [
 
-    {name: 'Dora' , id:1},
-    {name: 'Sema' , id:2},
-    {name: 'anoga' , id:3},
+    {name: 'Dora' , id:1,position:'Main Manager',salary:5600},
+    {name: 'Sema' , id:2 ,position:'Executive Manager',salary:5200},
+    {name: 'anoga' , id:3,position:'Engineer',salary:4300},
+    {name: 'diamon' , id:4,position:'Typer',salary:3000},
 ]
 app.get('/employees' ,(req,res)=>{
     res.status(200).send(employees)
@@ -19,7 +20,9 @@ app.get('/employees/:id',(req,res)=>{
 })
 app.post('/employees',(req,res)=>{
     const schema = Joi.object({
-        name: Joi.string().min(3).required()
+        name: Joi.string().min(3).required(),
+        position: Joi.string().min(3).required(),
+        salary : Joi.number().min(2500).max(5600)
     });
     const result =schema.validate(req.body)
    
@@ -29,7 +32,9 @@ app.post('/employees',(req,res)=>{
     }
     const employee = {
         id : employees.length + 1,
-        name : req.body.name 
+        name : req.body.name ,
+        position : req.body.position ,
+        salary : +req.body.salary ,
     }
     employees.push(employee);
     res.send(employee)
@@ -41,7 +46,9 @@ app.put('/employees/:id',(req,res)=>{
     if(!employee) res.status(404).send('there is no employee with this id !');
     // validate the new data 
     const schema = Joi.object({
-        name: Joi.string().min(3).required()
+        name: Joi.string().min(3).required(),
+        position: Joi.string().min(3).required(),
+        salary : Joi.number().min(2500).max(5600),
     });
 
     const result =schema.validate(req.body)
@@ -52,6 +59,8 @@ app.put('/employees/:id',(req,res)=>{
     }
     // if every thing ok change the entry 
     employee.name = req.body.name;
+    employee.position = req.body.position;
+    employee.salary = +req.body.salary;
     // send the response 
     res.send(employee);
 })
